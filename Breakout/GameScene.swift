3229ec3894319f040febb63974a8461583b1ba26
Ethan McHugh <ethanmchugh2024@gmail.com>
2022-07-28 09:42:09 -0500

    Initial Commit

---
+++ Breakout/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Breakout
+//
+//  Created by Ethan McHugh on 7/28/22.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+}
